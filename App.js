cb2480f2987f4e33fa56214a5353cce970704dde
firatcanbozkurt <firatcanbozkurt@hotmail.com>
2024-04-28 11:39:10 +0300

    push notification

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
+import NotificationProvider from "./components/context/NotificationProvider";
 import AuthProvider from "./components/context/AuthProvider.tsx";
 import AuthNavigator from "./navigatiors/AuthNavigator";
 import QueryProvider from "./components/context/QueryProvider";
@@ -10,13 +11,15 @@ function App() {
   return (
     <AuthProvider>
       <QueryProvider>
-        <GluestackUIProvider config={config}>
-          <SafeAreaProvider>
-            <NavigationContainer>
-              <AuthNavigator />
-            </NavigationContainer>
-          </SafeAreaProvider>
-        </GluestackUIProvider>
+        <NotificationProvider>
+          <GluestackUIProvider config={config}>
+            <SafeAreaProvider>
+              <NavigationContainer>
+                <AuthNavigator />
+              </NavigationContainer>
+            </SafeAreaProvider>
+          </GluestackUIProvider>
+        </NotificationProvider>
       </QueryProvider>
     </AuthProvider>
   );
