3d52b9383004320e78c6cb3a8e8264913c8f9905
halidacar <acarhalid1997@gmail.com>
2024-05-23 11:22:05 +0300

    fixed avatarIcon

--- screens/AllEventsScreen.js
+++ screens/AllEventsScreen.js
@@ -21,6 +21,7 @@ import {
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
 import LottieView from "lottie-react-native";
 import loadingAnimation from "../assets/loading.json";
+import AvatarIcon from "../components/AvatarIcon";
 
 const AllEventsScreen = ({ navigation }) => {
   const [loading, setLoading] = useState(true);
@@ -116,25 +117,7 @@ const AllEventsScreen = ({ navigation }) => {
             <ArrowLeftIcon size="20" color="white" />
           </TouchableOpacity>
           <Text className="pt-4 text-2xl font-semibold">EVENTS</Text>
-
-          <HStack
-            space="md"
-            h="100%"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Pressable onPress={() => navigation.navigate("Profile")}>
-              <View className="pr-5">
-                <Avatar size="md">
-                  <AvatarFallbackText>Halid Acar</AvatarFallbackText>
-                  <AvatarImage
-                    alt="HH"
-                    onPress={() => navigation.navigate("Profile")}
-                  />
-                </Avatar>
-              </View>
-            </Pressable>
-          </HStack>
+          <AvatarIcon />
         </View>
       </View>
       <FlatList
